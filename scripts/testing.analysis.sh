ae1210e7cc1233ba47fe74ad7d1a9f8831222eb1
Mathew Payne <geekmasher@gmail.com>
2020-12-15 15:19:50 +0000

    Remove script examples

--- scripts/testing.analysis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-../codeql/bin/codeql-cli/codeql database analyze \
-    --additional-packs ./queries/java \
-    --format csv \
-    --output /Users/geekmasher/Documents/development/security-debugging/results/java-lines_of_code.csv \
-    ../codeql/databases/WebGoat \
-    /Users/geekmasher/Documents/development/security-debugging/queries/java/LinesOfCode.ql
--- scripts/testing.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pipenv run python -m ghas_debugger \
-    --debug \
-    -b ../codeql/bin/codeql-cli/codeql \
-    -d ../codeql/databases/WebGoat
