c5e2de5742e5d7d352aa05e5bd4240013e9a7279
Mathew Payne <geekmasher@gmail.com>
2020-12-16 16:01:27 +0000

    Add lazier installing

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,8 +8,8 @@ mkdir -p .codeql
 git clone --depth=1 https://github.com/GeekMasher/codeql-debugger.git .codeql/debugger
 
 # Install deps
-python3 -m pip install --user pipenv
-./.local/bin/pipenv install --system
+python3 -m pip install jinja2
 
 # Run debugger
+PYTHONPATH="$PYTHONPATH:.codeql/debugger/"
 python3 .codeql/debugger/codeqldebugger
