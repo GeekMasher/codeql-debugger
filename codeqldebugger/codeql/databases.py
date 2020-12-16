a4c0925208700fe1ccd2768e97e6be24093ef65b
Mathew Payne <geekmasher@gmail.com>
2020-12-16 03:13:44 +0000

    Fix symlink issue

--- codeqldebugger/codeql/databases.py
+++ codeqldebugger/codeql/databases.py
@@ -24,7 +24,9 @@ def getDatabases(roots: list, name: str = None):
 
         for database_name in database_paths:
             database_path = os.path.join(root, database_name)
-            if not os.path.isdir(database_path):
+            if os.path.islink(database_path):
+                logging.debug("Symlink was found... Follow the white rabbit")
+            elif not os.path.isdir(database_path):
                 logging.debug("Database path is not a dir :: " + database_path)
                 continue
 
